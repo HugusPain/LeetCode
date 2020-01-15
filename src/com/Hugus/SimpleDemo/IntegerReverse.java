265be091da12793068795ab680476ef7fac71b66
HugusPain <13013722560@163.com>
2020-01-15 14:04:05 +0800

    02

---
+++ src/com/Hugus/SimpleDemo/IntegerReverse.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: IntegerReverse
+ * Author:   Administrator
+ * Date:     2020/1/15 0015 13:37
+ * Description: 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.Hugus.SimpleDemo;
+
+/**
+ *〈BriefDescription〉                                                                                    
+ * 〈给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。〉                                                                                                                                                               
+ *
+ * @author Administrator
+ * @create 2020/1/15 0015                                                                                        
+ * @since 1.0.0
+ */
+public class IntegerReverse {
+    public static void main(String[] args) {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        System.out.println("max:"+max);
+        System.out.println("min:"+min);
+        int a = 12345789;
+        int res = reverse(a);
+        System.out.println(res);
+    }
+
+    /*public static int reverse(int x){
+        int rev = 0;
+        int pop;
+        while (x != 0){
+            pop = x%10 ;
+            x = x / 10;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && x > 7))return 0;
+            if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && x < -8))return 0;
+            rev = rev*10+pop;
+        }
+
+        return rev;
+    }*/
+
+    public static int reverse(int x){
+        int rev = 0;
+        while (x != 0){
+            rev = rev*10+x%10;
+            x = x / 10;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && x > 7))return 0;
+            if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && x < -8))return 0;
+        }
+
+        return rev;
+    }
+
+
+
+
+}
